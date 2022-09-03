24fe0d4e1608b9c58a52e8e980104b1544c71c78
juanflarrotta <juanlarrotta@gmail.com>
2022-09-02 20:29:26 -0500

    Creacion carpetas del proyecto

---
+++ pages/api/menu.js
@@ -0,0 +1,25 @@
+const dataMenu = {
+  "lista": [
+    {
+      "card_list": [],
+      "pays_transfer": [
+        {
+          "between_cards": [],
+          "banks": [],
+          "credit_card": []
+        }
+      ],
+      "support": [
+        {
+          "lock": [],
+          "change_pass": []
+        }
+      ],
+      "enterprises": []
+    }
+  ]
+};
+
+export default function handler(req, res) {
+  res.status(200).json(dataMenu)
+}
