476892f2f92532ea4fa96a891205776790c39f88
juanflarrotta <juanlarrotta@gmail.com>
2022-09-02 22:59:50 -0500

    llamado al api, ejecucion del fetch

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { urls } from "../utils/constants";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
       <Header />
       <Footer />
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(urls.apiMenu)
+  const data = await res.json()
+
+  return { props: { data } }
 }
